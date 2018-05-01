efaa8434081153c2334f94fbd89c539de4b08abf
veetaw <veetaw@users.noreply.github.com>
2018-05-01 09:09:31 +0200

    hotfix & typo

--- script/generate_locale.bat
+++ script/generate_locale.bat
@@ -1,2 +1,4 @@
-flutter pub pub run intl_translation:extract_to_arb --output-dir=lib\src\l10n lib\src\locale\locale.dart
+cd ..
+
+flutter pub pub run intl_translation:extract_to_arb --output-dir=lib\src\l10n lib\src\locale\locales.dart
 flutter pub pub run intl_translation:generate_from_arb --output-dir=lib\src\l10n --no-use-deferred-loading lib\src\l10n\intl_*.arb lib\src\locale\locales.dart
