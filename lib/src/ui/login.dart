b1209c3b2de77eedc14eda6d55dcdde7386e2b89
veetaw <veetaw@users.noreply.github.com>
2018-05-02 18:55:43 +0200

    completed login ui

--- lib/src/ui/login.dart
+++ lib/src/ui/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../locale/locales.dart';
+
 final TextEditingController _schoolCodeController = new TextEditingController();
 final TextEditingController _usernameController = new TextEditingController();
 final TextEditingController _passwordController = new TextEditingController();
@@ -16,60 +18,118 @@ class Login extends StatefulWidget {
 class LoginState extends State<Login> {
   bool _obscured = true;
 
+  _verify() async {
+    String schoolCode = "ss${_schoolCodeController.text}",
+        username = _usernameController.text,
+        password = _passwordController.text;
+
+    if (schoolCode.length < 3 || username.isEmpty || password.isEmpty)
+      return false;
+    // todo initialize client
+  }
+
+  // todo fixme: bottom overflows on landscape
+  // probably it overflows on devices with a small screen
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
-        padding: const EdgeInsets.only(top: 24.0),
+        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
         child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
             new Align(
               child: new Text(
-                "Login", // todo localization
+                RELocalizations.of(context).login,
                 style: const TextStyle(
                   fontSize: 40.0,
                 ),
               ),
             ),
-            new TextField(
-              controller: _schoolCodeController,
-              decoration: new InputDecoration(
-                labelText: "School code", // todo localization
-                prefixText: 'ss',
-                prefixIcon: new Padding(
+            new Column(
+              children: <Widget>[
+                new Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: new Icon(Icons.school),
+                  child: new TextField(
+                    controller: _schoolCodeController,
+                    decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(8.0),
+                        ),
+                      ),
+                      labelText: RELocalizations.of(context).schoolCode,
+                      prefixText: 'ss',
+                      prefixIcon: new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Icon(
+                          Icons.school,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                new Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: new TextField(
+                    controller: _usernameController,
+                    decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(8.0),
+                        ),
+                      ),
+                      labelText: RELocalizations.of(context).username,
+                      prefixIcon: new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Icon(
+                          Icons.person,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: new TextField(
+                    controller: _passwordController,
+                    obscureText: _obscured,
+                    decoration: new InputDecoration(
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(8.0),
+                        ),
+                      ),
+                      labelText: RELocalizations.of(context).password,
+                      prefixIcon: new Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Icon(
+                          Icons.vpn_key,
+                        ),
+                      ),
+                      suffixIcon: new IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: new Icon(
+                          _obscured ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () => setState(
+                              () => _obscured = !_obscured,
+                            ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            new TextField(
-              controller: _usernameController,
-              decoration: new InputDecoration(
-                labelText: "Username", // todo localization
-                prefixIcon: new Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: new Icon(Icons.person),
-                ),
+            new FloatingActionButton(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              elevation: 0.0,
+              child: new Icon(
+                Icons.arrow_forward_ios,
               ),
-            ),
-            new TextField(
-              controller: _passwordController,
-              obscureText: _obscured,
-              decoration: new InputDecoration(
-                labelText: "password", // todo localization
-                prefixIcon: new Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: new Icon(Icons.vpn_key),
-                ),
-                suffixIcon: new IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: new Icon(
-                      _obscured ? Icons.visibility : Icons.visibility_off),
-                  onPressed: () => setState(() => _obscured = !_obscured),
-                ),
-              ),
+              onPressed: () {},
             ),
           ],
         ),
