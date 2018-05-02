f3640c4ece2a55eb75d606ca8141c1ef9b6040ff
veetaw <veetaw@users.noreply.github.com>
2018-05-02 18:00:35 +0200

    script edit

--- script/generate_locale.bat
+++ script/generate_locale.bat
@@ -1,4 +1,4 @@
 echo todo
 cd ..
 
-flutter pub pub run intl_translation:generate_from_arb --output-dir=lib\src\l10n --no-use-deferred-loading lib\src\l10n\intl_*.arb lib\src\locale\locales.dart
+flutter pub pub run intl_translation:generate_from_arb --output-dir=lib\src\l10n --no-use-deferred-loading lib\src\l10n\intl_en.arb lib\src\l10n\intl_it.arb lib\src\l10n\intl_messages.arb lib\src\locale\locales.dart
