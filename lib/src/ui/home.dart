c6bb3ff5ed9f5df060517c83366cd488f6099b5a
veetaw <veetaw@users.noreply.github.com>
2018-05-03 23:24:08 +0200

    initstate can't be async, fixed using a function and a futurebuilder

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:scuolanext/scuolanext.dart';
@@ -15,85 +17,111 @@ class HomeState extends State<Home> {
   final FlutterSecureStorage storage = new FlutterSecureStorage();
 
   @override
-  void initState() async {
+  void initState() {
+    _checkSession();
+    super.initState();
+  }
+
+  Future _checkSession() async {
     if (client.token == null || client.token.isEmpty) {
       await client.initClient(
         schoolCode: await storage.read(key: 'school_code'),
         token: await storage.read(key: 'token'),
       );
     }
-    super.initState();
   }
 
   // todo icons..
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text(
-                "${client.student.surname} ${client.student.name}",
+    return new FutureBuilder(
+      future: _checkSession(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return new Scaffold(
+              body: new Center(
+                child: new CircularProgressIndicator(),
               ),
-              accountEmail: null,
-              currentAccountPicture: new CircleAvatar(
-                child: new Icon(
-                  Icons.person,
+            );
+          default:
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text(
+                  RELocalizations.of(context).title,
                 ),
               ),
-            ),
-            new DrawerElement(
-              icon: Icons.book,
-              text: RELocalizations.of(context).absences,
-              onClick: () {},
-            ),
-            new DrawerElement(
-              icon: Icons.table_chart,
-              text: RELocalizations.of(context).timetable,
-              onClick: () {},
-            ),
-            new DrawerElement(
-              icon: Icons.looks_6,
-              text: RELocalizations.of(context).votes,
-              onClick: () {},
-            ),
-            new DrawerElement(
-              icon: Icons.calendar_today,
-              text: RELocalizations.of(context).homeworks,
-              onClick: () {},
-            ),
-            new Divider(),
-            // todo account
-            new DrawerElement(
-              icon: Icons.info,
-              text: 'info', // todo translation
-              onClick: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return new Text(
-                      "TODO",
-                    );
-                  },
-                );
-              },
-            ),
-            new DrawerElement(
-              icon: Icons.settings,
-              text: 'settings', // todo translation
-              onClick: () {},
-            ),
-            new Divider(),
-            new DrawerElement(
-              icon: Icons.exit_to_app,
-              text: 'exit', // todo translation
-              onClick: () {},
-            ),
-          ],
-        ),
-      ),
-      body: new Container(),
+              drawer: new Drawer(
+                child: new ListView(
+                  children: <Widget>[
+                    new UserAccountsDrawerHeader(
+                      accountName: new Text(
+                        "${client.student.surname} ${client.student.name}",
+                      ),
+                      accountEmail: null,
+                      currentAccountPicture: new CircleAvatar(
+                        child: new Icon(
+                          Icons.person,
+                        ),
+                      ),
+                    ),
+                    new DrawerElement(
+                      icon: Icons.book,
+                      text: RELocalizations.of(context).absences,
+                      onClick: () {},
+                    ),
+                    new DrawerElement(
+                      icon: Icons.table_chart,
+                      text: RELocalizations.of(context).timetable,
+                      onClick: () {},
+                    ),
+                    new DrawerElement(
+                      icon: Icons.looks_6,
+                      text: RELocalizations.of(context).votes,
+                      onClick: () {},
+                    ),
+                    new DrawerElement(
+                      icon: Icons.calendar_today,
+                      text: RELocalizations.of(context).homeworks,
+                      onClick: () {},
+                    ),
+                    new Divider(),
+                    // todo account
+                    new DrawerElement(
+                      icon: Icons.info,
+                      text: 'info', // todo translation
+                      onClick: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return new AlertDialog(
+                              title: new Text(
+                                "TODO",
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    new DrawerElement(
+                      icon: Icons.settings,
+                      text: 'settings', // todo translation
+                      onClick: () {},
+                    ),
+                    new Divider(),
+                    new DrawerElement(
+                      icon: Icons.exit_to_app,
+                      text: 'exit', // todo translation
+                      onClick: () {},
+                    ),
+                  ],
+                ),
+              ),
+              body: new Container(),
+            );
+        }
+      },
     );
   }
 }
