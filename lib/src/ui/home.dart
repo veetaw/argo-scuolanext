398aaac088d31769209c6f5d1d35fa4cc1d8bc02
veetaw <veetaw@users.noreply.github.com>
2018-05-06 11:48:20 +0200

    appbar & drawer restyle

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -48,76 +48,16 @@ class HomeState extends State<Home> {
           default:
             return new Scaffold(
               appBar: new AppBar(
+                backgroundColor: Colors.white,
+                centerTitle: true,
+                elevation: 1.0,
+                textTheme: Theme.of(context).textTheme,
+                iconTheme: Theme.of(context).iconTheme,
                 title: new Text(
                   RELocalizations.of(context).title,
                 ),
               ),
-              drawer: new Drawer(
-                child: new ListView(
-                  children: <Widget>[
-                    new UserAccountsDrawerHeader(
-                      accountName: new Text(
-                        "${client.student.surname} ${client.student.name}",
-                      ),
-                      accountEmail: null,
-                      currentAccountPicture: new CircleAvatar(
-                        child: new Icon(
-                          Icons.person,
-                        ),
-                      ),
-                    ),
-                    new DrawerElement(
-                      icon: Icons.book,
-                      text: RELocalizations.of(context).absences,
-                      onClick: () {},
-                    ),
-                    new DrawerElement(
-                      icon: Icons.table_chart,
-                      text: RELocalizations.of(context).timetable,
-                      onClick: () {},
-                    ),
-                    new DrawerElement(
-                      icon: Icons.looks_6,
-                      text: RELocalizations.of(context).votes,
-                      onClick: () {},
-                    ),
-                    new DrawerElement(
-                      icon: Icons.calendar_today,
-                      text: RELocalizations.of(context).homeworks,
-                      onClick: () {},
-                    ),
-                    new Divider(),
-                    // todo account
-                    new DrawerElement(
-                      icon: Icons.info,
-                      text: 'info', // todo translation
-                      onClick: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return new AlertDialog(
-                              title: new Text(
-                                "TODO",
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                    new DrawerElement(
-                      icon: Icons.settings,
-                      text: 'settings', // todo translation
-                      onClick: () {},
-                    ),
-                    new Divider(),
-                    new DrawerElement(
-                      icon: Icons.exit_to_app,
-                      text: 'exit', // todo translation
-                      onClick: () {},
-                    ),
-                  ],
-                ),
-              ),
+              drawer: new _Drawer(client),
               body: new Container(),
             );
         }
@@ -126,8 +66,92 @@ class HomeState extends State<Home> {
   }
 }
 
-class DrawerElement extends StatelessWidget {
-  DrawerElement({
+class _Drawer extends StatelessWidget {
+  final Client client;
+
+  _Drawer(this.client);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Drawer(
+      child: new ListView(
+        padding: const EdgeInsets.all(0.0),
+        children: <Widget>[
+          new UserAccountsDrawerHeader(
+            accountName: new Text(
+              "${client.student.surname} ${client.student.name}",
+              style: new TextStyle(
+                fontWeight: FontWeight.w500,
+                letterSpacing: .2,
+              ),
+            ),
+            accountEmail: null,
+            currentAccountPicture: new CircleAvatar(
+              child: new Text(
+                "${client.student.surname[0]} ${client.student.name[0]}",
+                style: new TextStyle(
+                  wordSpacing: 2.0,
+                ),
+              ),
+            ),
+          ),
+          new DrawerTile(
+            icon: Icons.book,
+            text: RELocalizations.of(context).absences,
+            onClick: () {},
+          ),
+          new DrawerTile(
+            icon: Icons.table_chart,
+            text: RELocalizations.of(context).timetable,
+            onClick: () {},
+          ),
+          new DrawerTile(
+            icon: Icons.looks_6,
+            text: RELocalizations.of(context).votes,
+            onClick: () {},
+          ),
+          new DrawerTile(
+            icon: Icons.calendar_today,
+            text: RELocalizations.of(context).homeworks,
+            onClick: () {},
+          ),
+          new Divider(),
+          // todo account
+          new DrawerTile(
+            icon: Icons.info,
+            text: 'info', // todo translation
+            onClick: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return new AlertDialog(
+                    title: new Text(
+                      "TODO",
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+          new DrawerTile(
+            icon: Icons.settings,
+            text: 'settings', // todo translation
+            onClick: () {},
+          ),
+          new Divider(),
+          new DrawerTile(
+            icon: Icons.exit_to_app,
+            text: 'exit', // todo translation
+            onClick: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerTile extends StatelessWidget {
+  DrawerTile({
     this.icon,
     this.text,
     this.onClick,
@@ -141,7 +165,13 @@ class DrawerElement extends StatelessWidget {
   Widget build(BuildContext context) {
     return new ListTile(
       leading: new Icon(icon),
-      title: new Text(text),
+      title: new Text(
+        text,
+        style: new TextStyle(
+          fontWeight: FontWeight.w500,
+          letterSpacing: .2,
+        ),
+      ),
       onTap: onClick,
     );
   }
